d4144a361c439ce9e9ab43dc669b91abb270a5c5
alexnro <alexjandron99@gmail.com>
2019-03-05 19:33:55 +0100

    Casos test creados y pasados

--- src/test/java/org/lasencinas/RectanguloTest.java
+++ src/test/java/org/lasencinas/RectanguloTest.java
@@ -2,13 +2,14 @@ package org.lasencinas;
 
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import org.lasencinas.rectangulo.Rectangulo;
 
 public class RectanguloTest {
 
-    Rectangulo rectanguloConstructorSuper = null;
-    Rectangulo rectanguloConstructorLocal = null;
-    Rectangulo rectanguloConstructorVacioSuper = null;
+    private Rectangulo rectanguloConstructorSuper = null;
+    private Rectangulo rectanguloConstructorLocal = null;
+    private Rectangulo rectanguloConstructorVacioSuper = null;
 
     @Before
     public void init() {
@@ -19,7 +20,42 @@ public class RectanguloTest {
 
     @Test
     public void constructorSuperTest() {
+        rectanguloConstructorSuper.setAltura(2);
+        rectanguloConstructorSuper.setAnchura(6);
+        assertEquals(12, rectanguloConstructorSuper.area(), 0);
 
+        rectanguloConstructorSuper.setAltura(3);
+        rectanguloConstructorSuper.setAnchura(20.2);
+        assertEquals(60.6, rectanguloConstructorSuper.area(), 0.1);
+
+        assertEquals("Desconocido", rectanguloConstructorSuper.getNombre());
+        rectanguloConstructorSuper.setNombre("rectangulo sale");
+        assertEquals("rectangulo sale", rectanguloConstructorSuper.getNombre());
     }
 
+    @Test
+    public void constructorLocalTest() {
+        rectanguloConstructorLocal.setAltura(6);
+        rectanguloConstructorLocal.setAnchura(8.3);
+        assertEquals(49.8, rectanguloConstructorLocal.area(), 0.1);
+
+        rectanguloConstructorLocal.setAltura(2);
+        rectanguloConstructorLocal.setAnchura(5);
+        assertEquals(10, rectanguloConstructorLocal.area(), 0);
+
+        assertEquals("rectangulo", rectanguloConstructorLocal.getNombre());
+        rectanguloConstructorLocal.setNombre("rectangulo sale");
+        assertEquals("rectangulo sale", rectanguloConstructorLocal.getNombre());
+    }
+
+    @Test
+    public void constructorVacioSuperTest() {
+        assertEquals(0, rectanguloConstructorVacioSuper.area(), 0);
+        assertEquals("Desconocido", rectanguloConstructorVacioSuper.getNombre());
+
+        rectanguloConstructorVacioSuper.setNombre("rectangulo sale");
+        assertEquals("rectangulo sale", rectanguloConstructorVacioSuper.getNombre());
+
+        assertEquals(0, rectanguloConstructorVacioSuper.area(), 0);
+    }
 }
