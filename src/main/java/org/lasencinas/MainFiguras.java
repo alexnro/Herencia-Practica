d53debd4c20162f3188ed06d2934e53a4cbecd2b
alexnro <alexjandron99@gmail.com>
2019-03-05 17:14:10 +0100

    Creado proyecto maven

---
+++ src/main/java/org/lasencinas/MainFiguras.java
@@ -0,0 +1,6 @@
+package org.lasencinas;
+
+
+public class MainFiguras  {
+
+}
---
+++ src/test/java/org/lasencinas/MainFigurasTest.java
@@ -0,0 +1,20 @@
+package org.lasencinas;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple MainFiguras.
+ */
+public class MainFigurasTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
