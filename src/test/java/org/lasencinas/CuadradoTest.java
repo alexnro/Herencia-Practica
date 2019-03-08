ffc1536e4ce9db663561706e49f4e508fbb89192
alexnro <alexjandron99@gmail.com>
2019-03-08 19:34:41 +0100

    Caso test creado y pasado

--- src/test/java/org/lasencinas/CuadradoTest.java
+++ src/test/java/org/lasencinas/CuadradoTest.java
@@ -1,4 +1,45 @@
 package org.lasencinas;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.lasencinas.cuadrado.Cuadrado;
+
+import static org.junit.Assert.assertEquals;
+
 public class CuadradoTest {
+
+    private Cuadrado cuadradoConstructorSuper = null;
+    private Cuadrado cuadradoConstructorLocal = null;
+    private Cuadrado cuadradoConstructorVacio = null;
+
+    @Before
+    public void init() {
+        cuadradoConstructorSuper = new Cuadrado(0);
+        cuadradoConstructorLocal = new Cuadrado("cuadrado", 0);
+        cuadradoConstructorVacio = new Cuadrado();
+    }
+
+    @Test
+    public void constructorSuperclaseTest() {
+        assertEquals(0, cuadradoConstructorSuper.area(), 0);
+        assertEquals("Desconocido", cuadradoConstructorSuper.getNombre());
+
+        cuadradoConstructorSuper.setNombre("cuadrado");
+        cuadradoConstructorSuper.setLado(6);
+        assertEquals("cuadrado", cuadradoConstructorSuper.getNombre());
+        assertEquals(36, cuadradoConstructorSuper.area(), 0);
+    }
+
+    @Test
+    public void constructorLocalTest() {
+        assertEquals("cuadrado", cuadradoConstructorLocal.getNombre());
+
+        cuadradoConstructorLocal.setNombre("nombre");
+        assertEquals("nombre", cuadradoConstructorLocal.getNombre());
+    }
+
+    @Test
+    public void constructorVacioTest() {
+        assertEquals("Desconocido", cuadradoConstructorVacio.getNombre());
+    }
 }
